fec28f6a51e97c6a185d06ce21b966a05f336c1a
madhurpokarna <madhurpokarna@gmail.com>
2025-05-21 19:48:36 +0530

    unit test case updated

---
+++ tests/test_operations.py
@@ -0,0 +1,12 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+def test_sub():
+    assert sub(5,2)==3
+    assert sub(4,1)==3
+    assert sub(8,3)==5
+    assert sub(3,3)==0
+    
